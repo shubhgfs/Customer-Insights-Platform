5989a0a189b58d2eadd6ed103712980d088d6aff
Shubh Mehta <shubh.mehta@greenstone.com.au>
2025-05-19 21:13:24 +1000

    Refactor SQL and Transcription Agents to utilize Azure OpenAI embedder and streamline knowledge base creation

--- agno_test.py
+++ agno_test.py
@@ -15,6 +15,16 @@ from agno.vectordb.search import SearchType
 from agno.knowledge.combined import CombinedKnowledgeBase
 from agno.vectordb.weaviate import Distance, VectorIndex, Weaviate
 from agno.team.team import Team
+from agno.vectordb.chroma import ChromaDb
+from agno.embedder.azure_openai import AzureOpenAIEmbedder
+
+load_dotenv()
+
+os.environ["AZURE_EMBEDDER_OPENAI_API_KEY"] = os.getenv("AZURE_OPENAI_API_KEY_AQMAGENTICOS")
+os.environ["AZURE_EMBEDDER_OPENAI_ENDPOINT"] = os.getenv("AZURE_OPENAI_ENDPOINT_AQMAGENTICOS")
+os.environ["AZURE_EMBEDDER_DEPLOYMENT"] = "o3-mini"
+
+embedder = AzureOpenAIEmbedder()
 
 storage = SqliteStorage(
     table_name="tblMaster_CIP",
@@ -25,56 +35,17 @@ with open(r'sql_agent_config.json', 'r') as f:
     file = json.load(f)
     sql_agent_config = file['agent_config']
 
-load_dotenv()
-
-api_key = os.getenv("AZURE_OPENAI_API_KEY_AQMAGENTICOS")
-azure_endpoint = os.getenv("AZURE_OPENAI_ENDPOINT_AQMAGENTICOS")
-azure_deployment = "o3-mini"
-api_version = "2024-12-01-preview"
-
-def create_knowledge_base(path, collection):
-    documents = []
-    
-    for file in os.listdir(path):
-        if file.endswith(".pdf"):
-            full_path = os.path.join(path, file).replace("\\", "/").replace(" ", "_")
-            folders = path.split("/")[-3:]
-            metadata = {
-                "brand": folders[0],
-                "product": folders[1],
-                "sale_status": folders[2],
-                "year": file[:4],
-                "month": file[4:6],
-                "day": file[6:8],
-            }
-            documents.append({
-                "path": full_path,
-                "metadata": metadata
-            })
-
-    vector_db = Weaviate(
-        collection=collection,
-        search_type=SearchType.hybrid,
-        distance=Distance.COSINE,
-        vector_index=VectorIndex.HNSW,
-        local=True,
-    )
-    
-    knowledge_base = PDFKnowledgeBase(
-        name=collection,
-        path=path,
-        vector_db=vector_db,
-    )
-
-    knowledge_base.load(recreate=True)
-    return knowledge_base
-
 azure_model = AzureOpenAI(
-    api_key=api_key,
-    azure_endpoint=azure_endpoint,
-    azure_deployment=azure_deployment,
-    api_version=api_version,
+    api_key=os.getenv("AZURE_OPENAI_API_KEY_AQMAGENTICOS"),
+    azure_endpoint=os.getenv("AZURE_OPENAI_ENDPOINT_AQMAGENTICOS"),
+    azure_deployment="o3-mini",
+    api_version="2024-12-01-preview",
 )
+
+
+#######################################################################
+####################     SQL AGENT     ################################
+#######################################################################
 
 knowledge_base = JSONKnowledgeBase(
     path = r"knowledge",
@@ -84,38 +55,10 @@ knowledge_base = JSONKnowledgeBase(
     distance=Distance.COSINE,
     vector_index=VectorIndex.HNSW,
     local=True,
+    embedder=embedder,
     )
 )
-
-all_kb = [
-    [r'C:/Users/shubhm01/Documents/Customer Insights Platform/call transcriptions/ASIA/Life/No Sale', "asia_life_no_sale"],
-    [r'C:/Users/shubhm01/Documents/Customer Insights Platform/call transcriptions/ASIA/Life/Sale', "asia_life_sale"],
-    [r'C:/Users/shubhm01/Documents/Customer Insights Platform/call transcriptions/OneChoice/Life/Sale', "onechoice_life_sale"],
-    [r'C:/Users/shubhm01/Documents/Customer Insights Platform/call transcriptions/OneChoice/Life/No Sale', "onechoice_life_no_sale"],
-    [r'C:/Users/shubhm01/Documents/Customer Insights Platform/call transcriptions/OneChoice/Income Protection/Sale', "onechoice_income_protection_sale"],
-    [r'C:/Users/shubhm01/Documents/Customer Insights Platform/call transcriptions/OneChoice/Income Protection/No Sale', "onechoice_income_protection_no_sale"],
-    [r'C:/Users/shubhm01/Documents/Customer Insights Platform/call transcriptions/Real/Life/Sale', "real_life_sale"],
-    [r'C:/Users/shubhm01/Documents/Customer Insights Platform/call transcriptions/Real/Life/No Sale', "real_life_no_sale"],
-    [r'C:/Users/shubhm01/Documents/Customer Insights Platform/call transcriptions/Real/Funeral/Sale', "real_funeral_sale"],
-    [r'C:/Users/shubhm01/Documents/Customer Insights Platform/call transcriptions/Real/Funeral/No Sale', "real_funeral_no_sale"],
-    [r'C:/Users/shubhm01/Documents/Customer Insights Platform/call transcriptions/Real/Income Protection/Sale', "real_income_protection_sale"],
-    [r'C:/Users/shubhm01/Documents/Customer Insights Platform/call transcriptions/Real/Income Protection/No Sale', "real_income_protection_no_sale"],
-]
-
-transcription_knowledge_bases = CombinedKnowledgeBase(
-    sources = [
-        create_knowledge_base(path, collection)
-        for path, collection in all_kb
-    ],
-    vector_db=Weaviate(
-        collection="transcription master",
-        search_type=SearchType.hybrid,
-        distance=Distance.COSINE,
-        vector_index=VectorIndex.HNSW,
-        local=True,
-    )
-)
-
+print('sql agent knowledge base loaded')
 knowledge_tool = KnowledgeTools(
         knowledge=knowledge_base,
         think=True,
@@ -130,16 +73,6 @@ thinking_tool = ThinkingTools(
     think=True,
     instructions=sql_agent_config.get('instructions', None),
     add_instructions=True,
-)
-
-transcription_knowledge_tool = KnowledgeTools(
-    knowledge=transcription_knowledge_bases,
-    think=True,
-    search=True,
-    analyze=True,
-    instructions="",
-    add_instructions=True,
-    add_few_shot=True,
 )
 
 sql_agent = Agent(
@@ -179,6 +112,89 @@ sql_agent = Agent(
     monitoring=True,
 )
 
+
+#######################################################################
+####################     TRANSCRIPTION AGENT     ######################
+#######################################################################
+
+print('starting transcription agent')
+def create_knowledge_base(path, collection):
+    documents = []
+    print(f'creating knowledge base for {collection}')
+    for file in os.listdir(path):
+        if file.endswith(".pdf"):
+            full_path = os.path.join(path, file).replace("\\", "/").replace(" ", "_")
+            folders = path.split("/")[-3:]
+            metadata = {
+                "brand": folders[0],
+                "product": folders[1],
+                "sale_status": folders[2],
+                "year": file[:4],
+                "month": file[4:6],
+                "day": file[6:8],
+            }
+            documents.append({
+                "path": full_path,
+                "metadata": metadata
+            })
+
+    vector_db = Weaviate(
+        collection=collection,
+        search_type=SearchType.hybrid,
+        distance=Distance.COSINE,
+        vector_index=VectorIndex.HNSW,
+        embedder=embedder,
+    )
+    
+    knowledge_base = PDFKnowledgeBase(
+        name=collection,
+        path=path,
+        vector_db=vector_db,
+    )
+
+    knowledge_base.load(recreate=True)
+    return knowledge_base
+
+all_kb = [
+    [r'C:/Users/shubhm01/Documents/Customer Insights Platform/call transcriptions/ASIA/Life/No Sale', "asia_life_no_sale"],
+    [r'C:/Users/shubhm01/Documents/Customer Insights Platform/call transcriptions/ASIA/Life/Sale', "asia_life_sale"],
+    [r'C:/Users/shubhm01/Documents/Customer Insights Platform/call transcriptions/OneChoice/Life/Sale', "onechoice_life_sale"],
+    [r'C:/Users/shubhm01/Documents/Customer Insights Platform/call transcriptions/OneChoice/Life/No Sale', "onechoice_life_no_sale"],
+    [r'C:/Users/shubhm01/Documents/Customer Insights Platform/call transcriptions/OneChoice/Income Protection/Sale', "onechoice_income_protection_sale"],
+    [r'C:/Users/shubhm01/Documents/Customer Insights Platform/call transcriptions/OneChoice/Income Protection/No Sale', "onechoice_income_protection_no_sale"],
+    [r'C:/Users/shubhm01/Documents/Customer Insights Platform/call transcriptions/Real/Life/Sale', "real_life_sale"],
+    [r'C:/Users/shubhm01/Documents/Customer Insights Platform/call transcriptions/Real/Life/No Sale', "real_life_no_sale"],
+    [r'C:/Users/shubhm01/Documents/Customer Insights Platform/call transcriptions/Real/Funeral/Sale', "real_funeral_sale"],
+    [r'C:/Users/shubhm01/Documents/Customer Insights Platform/call transcriptions/Real/Funeral/No Sale', "real_funeral_no_sale"],
+    [r'C:/Users/shubhm01/Documents/Customer Insights Platform/call transcriptions/Real/Income Protection/Sale', "real_income_protection_sale"],
+    [r'C:/Users/shubhm01/Documents/Customer Insights Platform/call transcriptions/Real/Income Protection/No Sale', "real_income_protection_no_sale"],
+]
+print('creating knowledge base for transcription agent')
+transcription_knowledge_bases = CombinedKnowledgeBase(
+    sources = [
+        create_knowledge_base(path, collection)
+        for path, collection in all_kb
+    ],
+    vector_db=Weaviate(
+        collection="transcription master",
+        search_type=SearchType.hybrid,
+        distance=Distance.COSINE,
+        vector_index=VectorIndex.HNSW,
+        embedder=embedder,
+        # local=True,
+    )
+)
+print('knowledge base for transcription agent created')
+transcription_knowledge_tool = KnowledgeTools(
+    knowledge=transcription_knowledge_bases,
+    think=True,
+    search=True,
+    analyze=True,
+    instructions="",
+    add_instructions=True,
+    add_few_shot=True,
+)
+
 transcript_agent = Agent(
     name="Transcript Reasoning Agent",
     model=azure_model,
@@ -197,7 +213,6 @@ transcript_agent = Agent(
     num_history_runs=10,
     storage=storage,
     show_tool_calls=True,
-    reasoning=True,  # This agent should reason through citations
     read_chat_history=True,
     read_tool_call_history=True,
     system_message_role="system",
@@ -213,6 +228,7 @@ transcript_agent = Agent(
     add_state_in_messages=True,
     monitoring=True,
 )
+
 
 
 customer_insight_team = Team(
@@ -234,7 +250,7 @@ customer_insight_team = Team(
     show_members_responses=True,
 )
 
-app = Playground(agents = [sql_agent, transcript_agent], teams=[customer_insight_team]).get_app()
+app = Playground(teams=[customer_insight_team]).get_app()
 
 
 if __name__ == "__main__":
